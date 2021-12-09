9a37be5fef190c4c98a90d590c785f2b574d4d28
Albert <111111@111.com>
2021-12-09 13:50:44 +0800

    提交

--- src/test/java/com/binance/client/examples/market/GetExchangeInformation.java
+++ src/test/java/com/binance/client/examples/market/GetExchangeInformation.java
@@ -1,15 +1,19 @@
 package com.binance.client.examples.market;
 
+import com.alibaba.fastjson.JSONObject;
 import com.binance.client.RequestOptions;
 import com.binance.client.SyncRequestClient;
 
 import com.binance.client.examples.constants.PrivateConfig;
+import com.binance.client.model.market.ExchangeInformation;
 
 public class GetExchangeInformation {
     public static void main(String[] args) {
         RequestOptions options = new RequestOptions();
         SyncRequestClient syncRequestClient = SyncRequestClient.create(PrivateConfig.API_KEY, PrivateConfig.SECRET_KEY,
                 options);
-        System.out.println(syncRequestClient.getExchangeInformation());
+        ExchangeInformation ei  = syncRequestClient.getExchangeInformation();
+        System.out.println(ei);
+        System.out.println(JSONObject.toJSONString(ei));
     }
 }
